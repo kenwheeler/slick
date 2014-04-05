822dc6c448dd3ce47f8d360d273038dea5f7f88f
Mo Samman <moesef1@gmail.com>
2014-04-04 22:08:58 -0700

    Improvement: Slide addition and removal
    -add appendToFront option
    -add slickAddAt and slickRemoveAt public methods
    -changed slickAdd and slickRemove private methods

--- js/scripts.js
+++ js/scripts.js
@@ -93,7 +93,7 @@ $(document).ready(function(){
   });
 
   $('.js-remove-slide').on('click', function(){
-    $('.add-remove').slickRemove(slideIndex - 1);
+    $('.add-remove').slickRemove();
     slideIndex--;
   });
 
