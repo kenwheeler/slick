e7b6fbf2d11f5fc29daa9aec63d49a0860dd8618
Túbal Martín <tubal.martin@hola-internet.com>
2014-06-03 18:20:26 +0200

    Bump version to 1.3.7

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.3.6
+ Version: 1.3.7
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
