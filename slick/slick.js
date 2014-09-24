f9c530be33b02d3316d35eaa5f20e4f12b268c53
kenwheeler <dubmediagroup@gmail.com>
2014-09-23 23:46:43 -0400

    Lazy load less than slidesToShow

--- slick/slick.js
+++ slick/slick.js
@@ -931,7 +931,7 @@
         loadRange = _.$slider.find('.slick-slide').slice(rangeStart, rangeEnd);
         loadImages(loadRange);
 
-          if (_.slideCount == 1){
+          if (_.slideCount <= _.options.slidesToShow){
               cloneRange = _.$slider.find('.slick-slide')
               loadImages(cloneRange)
           }else
