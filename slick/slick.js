bac96049c6bb9205dbd8b8eb2b96869fb69f4fc5
kenwheeler <dubmediagroup@gmail.com>
2014-10-03 06:39:17 -0400

    Fixing offsetLeft of null error

--- slick/slick.js
+++ slick/slick.js
@@ -751,15 +751,16 @@
         }
 
         if (_.options.variableWidth === true) {
+
             if(_.slideCount <= _.options.slidesToShow || _.options.infinite === false) {
                 targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex);
             } else {
                 targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex + _.options.slidesToShow);
             }
-            targetLeft = targetSlide[0].offsetLeft * -1;
+            targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
             if (_.options.centerMode === true) {
                 targetSlide = _.$slideTrack.children('.slick-slide').eq(slideIndex + _.options.slidesToShow + 1);
-                targetLeft = targetSlide[0].offsetLeft * -1;
+                targetLeft = targetSlide[0] ? targetSlide[0].offsetLeft * -1 : 0;
                 targetLeft += (_.$list.width() - targetSlide.outerWidth()) / 2;
             }
         }
