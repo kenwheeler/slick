54b6e022ea40868a1ba3501f5b60103e47064814
kenwheeler <dubmediagroup@gmail.com>
2014-10-05 07:46:38 -0400

    Bumping version because npm is a fuckin dork

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.3.8
+ Version: 1.3.9
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
