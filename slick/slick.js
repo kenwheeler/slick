b7c551af91c37f158705f3682aaf97b2834834b4
kenwheeler <dubmediagroup@gmail.com>
2014-10-07 16:12:16 -0400

    Version bump

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.3.9
+ Version: 1.3.10
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
