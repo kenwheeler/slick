669e89246751cd9384d113f8eee830d208457b85
hereandnow <hereandnow78@gmail.com>
2015-04-21 16:52:10 +0200

    fix slick-active

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.5.1
+ Version: 1.5.1-fix
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
@@ -1453,7 +1453,7 @@
             $(_.$slideTrack).children().on('click.slick', _.selectHandler);
         }
 
-        _.setSlideClasses(0);
+        _.setSlideClasses(_.currentSlide);
 
         _.setPosition();
 
