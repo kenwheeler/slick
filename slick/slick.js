f73f1e4647c5c4d3fd7ed6c5efea64c10340d2e3
Ken Wheeler <ken_wheeler@me.com>
2015-05-13 03:35:31 -0400

    1.5.5 Hotfix

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.5.4
+ Version: 1.5.5
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
@@ -192,9 +192,9 @@
             // Extracted from jQuery v1.11 source
             _.htmlExpr = /^(?:\s*(<[\w\W]+>)[^>]*)$/;
 
-            _.checkResponsive(true);
-
             _.init(true);
+
+            _.checkResponsive(true);
 
         }
 
