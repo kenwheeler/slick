eadbc53b205bd796eab4a0d08de0c44e03bd5d8d
Mohamed Rafi <mmohamedrafi@tiaa-cref.org>
2015-07-02 23:47:55 -0400

    Comments removed

--- slick/slick.js
+++ slick/slick.js
@@ -45,7 +45,7 @@
                 appendDots: $(element),
                 arrows: true,
                 asNavFor: null,
-               prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button">Previous</button>',
+                prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button">Previous</button>',
                 nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button">Next</button>',
                 autoplay: false,
                 autoplaySpeed: 3000,
@@ -526,10 +526,6 @@
 
         _.updateDots();
 
-        // This is controlled through _.activeADA method
-        //if (_.options.accessibility === true) {
-        //    _.$list.prop('tabIndex', 0);
-        //}
 
         _.setSlideClasses(typeof _.currentSlide === 'number' ? _.currentSlide : 0);
 
