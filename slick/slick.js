1b8c4b96f75ec9b78faf27264958e2c6c1cea966
Marcell Scarlett <marcell@marcellscarlett.com>
2015-08-11 20:01:43 -0400

    keep default behavior take 2

--- slick/slick.js
+++ slick/slick.js
@@ -81,7 +81,7 @@
                 touchMove: true,
                 touchThreshold: 5,
                 useCSS: true,
-                useTransform: true,
+                useTransform: false,
                 variableWidth: false,
                 vertical: false,
                 verticalSwiping: false,
