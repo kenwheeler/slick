ac31d6a450548d56e4b60bd1cb2749f05a7cc65a
nevace <liam.neville89@gmail.com>
2015-11-25 10:56:46 +0000

    changed inline width style to min-width to fix ios flexbox issue

--- slick/slick.js
+++ slick/slick.js
@@ -1769,7 +1769,9 @@
         }
 
         var offset = _.$slides.first().outerWidth(true) - _.$slides.first().width();
-        if (_.options.variableWidth === false) _.$slideTrack.children('.slick-slide').width(_.slideWidth - offset);
+        if (_.options.variableWidth === false) _.$slideTrack.children('.slick-slide').css({
+      'min-width': _.slideWidth - offset
+    });
 
     };
 
