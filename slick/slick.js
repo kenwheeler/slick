4eefb2174a3573a8db7043c0bb852173f8c50f55
Rishanthan <rishanthakumarr@zone24x7.com>
2017-01-19 13:29:50 +0530

    Fix for lazyload on demand with dots unexpected behaviour.

--- slick/slick.js
+++ slick/slick.js
@@ -1453,7 +1453,7 @@
     Slick.prototype.lazyLoad = function() {
 
         var _ = this,
-            loadRange, cloneRange, rangeStart, rangeEnd;
+            loadRange, cloneRange, rangeStart, rangeEnd, slideToBegin;
 
         function loadImages(imagesScope) {
 
@@ -1518,7 +1518,8 @@
                 rangeEnd = 2 + (_.options.slidesToShow / 2 + 1) + _.currentSlide;
             }
         } else {
-            rangeStart = _.options.infinite ? _.options.slidesToShow + _.currentSlide : _.currentSlide;
+            slideToBegin =  _.currentSlide - Math.max(0, _.currentSlide + _.options.slidesToShow - _.slideCount);
+            rangeStart = _.options.infinite ? _.options.slidesToShow + slideToBegin : slideToBegin;
             rangeEnd = Math.ceil(rangeStart + _.options.slidesToShow);
             if (_.options.fade === true) {
                 if (rangeStart > 0) rangeStart--;
