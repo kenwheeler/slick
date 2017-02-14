7fc226f6b6e1b6683e3cc5e2a543cdc8e402a4fa
valerijv <valerij.vasilcenko@gmail.com>
2017-02-14 11:55:19 +0200

    Swipe left bug on not infinite slider
    When slides to swipe left are more than actual swipes, index will be to big and slide will not occur. This fixes the case.

--- slick/slick.js
+++ slick/slick.js
@@ -744,6 +744,10 @@
                 prevNavigable = navigables[n];
             }
         }
+         
+        if (index + _.options.slidesToShow > navigables.length) {
+            index = navigables.length - _.options.slidesToShow;
+        }
 
         return index;
     };
