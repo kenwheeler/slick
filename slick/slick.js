d8b4f723316a7cf823b0ddd3185cd323dd513eb0
Dorian Kavaja <dorik2k@gmail.com>
2017-05-02 16:05:42 +0200

    Center children if they fit in one slide (very rough)
    If there are no slides (children are all shown), set width relative to the number in percent so they all fill the entire track. Then the elements inside can be centred,

--- slick/slick.js
+++ slick/slick.js
@@ -1982,7 +1982,17 @@
 
         if (_.options.vertical === false && _.options.variableWidth === false) {
             _.slideWidth = Math.ceil(_.listWidth / _.options.slidesToShow);
-            _.$slideTrack.width(Math.ceil((_.slideWidth * _.$slideTrack.children('.slick-slide').length)));
+             
+            if (_.slideCount < _.options.slidesToShow) {
+                _.$slideTrack.width("100%");
+
+                _.$slideTrack.children('.slick-slide').width(Math.floor(100 / _.$slideTrack.children('.slick-slide').length) + "%");
+            } else {
+                _.$slideTrack.width(Math.ceil((_.slideWidth * _.$slideTrack.children('.slick-slide').length)));
+
+                var offset = _.$slides.first().outerWidth(true) - _.$slides.first().width();
+                if (_.options.variableWidth === false) _.$slideTrack.children('.slick-slide').width(_.slideWidth - offset);
+            }
 
         } else if (_.options.variableWidth === true) {
             _.$slideTrack.width(5000 * _.slideCount);
@@ -1990,9 +2000,6 @@
             _.slideWidth = Math.ceil(_.listWidth);
             _.$slideTrack.height(Math.ceil((_.$slides.first().outerHeight(true) * _.$slideTrack.children('.slick-slide').length)));
         }
-
-        var offset = _.$slides.first().outerWidth(true) - _.$slides.first().width();
-        if (_.options.variableWidth === false) _.$slideTrack.children('.slick-slide').width(_.slideWidth - offset);
 
     };
 
