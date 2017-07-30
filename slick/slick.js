77101f1dc3e72cb44c253c81a74f9d4881b093a6
Greg Westneat <greg@fosterwebmarketing.com>
2017-07-30 12:19:13 -0700

    1.7.1

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.6.0
+ Version: 1.7.1
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
