ee7d37faeb92c4619ffeefeba2cc4c733f39b1b3
Ken Wheeler <thekenwheeler@gmail.com>
2017-10-03 13:51:17 -0400

    versions

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.8.0
+ Version: 1.8.1
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
