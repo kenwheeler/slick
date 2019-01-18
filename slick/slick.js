a3131ca42004e32706928d645567000f2ce060eb
Nick Marnik <nkmhockey@gmail.com>
2019-01-18 10:35:48 -0500

    Revert "1.9.0"
    This reverts commit a2aa3fec335c50aceb58f6ef6d22df8e5f3238e1.

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.9.0
+ Version: 1.8.1
   Author: Ken Wheeler
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
@@ -1019,7 +1019,7 @@
             .off('focus.slick blur.slick')
             .on(
                 'focus.slick',
-                '*',
+                '*', 
                 function(event) {
                     var $sf = $(this);
 
@@ -1034,7 +1034,7 @@
                 }
             ).on(
                 'blur.slick',
-                '*',
+                '*', 
                 function(event) {
                     var $sf = $(this);
 
