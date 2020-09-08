d3aedb9028145e3e688d5f08f29211ed81595c45
Guillaume Plante <guillaume.plante@nurun.com>
2020-09-08 12:13:34 -0400

    Set dot buttons tabindex to "0" for tab navigation

--- slick/slick.js
+++ slick/slick.js
@@ -1370,7 +1370,7 @@
                     'aria-controls': 'slick-slide' + _.instanceUid + mappedSlideIndex,
                     'aria-label': (i + 1) + ' of ' + numDotGroups,
                     'aria-selected': null,
-                    'tabindex': '-1'
+                    'tabindex': '0'
                 });
 
             }).eq(_.currentSlide).find('button').attr({
