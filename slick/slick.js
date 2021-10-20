65895db49daf9af816426a81bfa5720501a5b480
somethumb <somethumb@users.noreply.github.com>
2021-10-19 19:53:17 -0700

    Update slick.js

--- slick/slick.js
+++ slick/slick.js
@@ -6,7 +6,7 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.8.13
+ Version: 1.8.14
   Author: Ken Wheeler - Fix dot by Somethumb
  Website: http://kenwheeler.github.io
     Docs: http://kenwheeler.github.io/slick
